84eddf64272da66305c32f9f15bd7f0093e97967
Introsent <72797970+Introsent@users.noreply.github.com>
2022-05-28 14:14:17 +0300

    Дз за 6 урок Продвинутого JS
    1. Вынести поиск в отдельный компонент.
    2. Вынести корзину в отдельный компонент

---
+++ js/SearchComp.js
@@ -0,0 +1,13 @@
+Vue.component('searchline', {
+    data(){
+        return {
+            userSearch: '',
+        }
+    },
+    template: `<form action="#" class="search-form" @submit.prevent="filter">
+                <input type="text" class="search-field" v-model="userSearch">
+                <button @click="$root.$refs.products.filter(userSearch)" class="btn-search" type="submit">
+                    <i class="fas fa-search"></i>
+                </button>
+            </form>`
+})
